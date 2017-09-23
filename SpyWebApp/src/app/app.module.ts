3145c9b46a1b95982c6708acc126b670105c2797
Salvo Bertoncini <salvobertoncini@users.noreply.github.com>
2017-09-23 17:08:23 +0200

    Right Menu

--- SpyWebApp/src/app/app.module.ts
+++ SpyWebApp/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+
+import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
