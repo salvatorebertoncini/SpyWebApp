fcf317c04bc630fa379c8acfe8e093103615c841
Salvo Bertoncini <salvobertoncini@users.noreply.github.com>
2017-09-23 17:46:12 +0200

    cards example in main content

--- SpyWebApp/src/app/app.module.ts
+++ SpyWebApp/src/app/app.module.ts
@@ -8,10 +8,14 @@ import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { SpytoolbarComponent } from './spytoolbar/spytoolbar.component';
+import { SpyContentComponent } from './spycontent/spy-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpytoolbarComponent,
+    SpyContentComponent
   ],
   imports: [
     BrowserModule,
